0d1706c79e7d8a1bfbcaabf4667895dd240c2f4b
Damian Brady <dev@damianbrady.com.au>
2015-11-08 13:06:03 -0800

    Explicit exception logging

--- AnimalWars/Data/AnimalWarsData.cs
+++ AnimalWars/Data/AnimalWarsData.cs
@@ -3,37 +3,59 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using AnimalWars.Models;
 using Dapper;
+using Microsoft.ApplicationInsights;
 
 namespace AnimalWars.Data
 {
     public class AnimalWarsData
     {
         private SqlConnection _cnn;
+        private TelemetryClient _telemetryClient;
 
         public AnimalWarsData()
         {
             _cnn = new SqlConnection(ConfigurationManager.ConnectionStrings["AnimalWarsConnection"].ConnectionString);
+            _telemetryClient = new TelemetryClient();
         }
 
 
         public AnimalWar GetSingleWar(Category animalCategory)
         {
-            var query = "SELECT TOP 2 Id, Name, Category, Count FROM Animals WHERE Category = @category ORDER BY newid()";
-            var animals = _cnn.Query<Animal>(query, new {category = animalCategory.ToString()}).ToList();
-            return new AnimalWar
+            try
             {
-                AnimalOne = animals.First(),
-                AnimalTwo = animals.ElementAt(1)
-            };
+                var query =
+                    "SELECT TOP 2 Id, Name, Category, Count FROM Animals WHERE Category = @category ORDER BY newid()";
+                var animals = _cnn.Query<Animal>(query, new { category = animalCategory.ToString() }).ToList();
+                return new AnimalWar
+                {
+                    AnimalOne = animals.First(),
+                    AnimalTwo = animals.ElementAt(1)
+                };
+            }
+            catch (Exception ex)
+            {
+                _telemetryClient.TrackException(ex);
+                throw;
+            }
         }
 
         public void IncrementCount(string id)
         {
-            var query = "UPDATE Animals SET Count = Count + 1 WHERE Id = @id";
-            _cnn.Execute(query, new {id});
+            try
+            {
+                var query = "UPDATE Animals SET Count = Count + 1 WHERE Id = @id";
+                _cnn.Execute(query, new { id });
+            }
+            catch (Exception ex)
+            {
+                _telemetryClient.TrackException(ex);
+                throw;
+            }
+
         }
     }
 }
