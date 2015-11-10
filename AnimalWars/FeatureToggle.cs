30d7a1b62b4763d27e52a96314694a2fd8d6f9b9
Damian Brady <dev@damianbrady.com.au>
2015-11-10 12:26:05 -0800

    Added feature toggle

---
+++ AnimalWars/FeatureToggle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace AnimalWars
+{
+    public static class FeatureToggle
+    {
+        public static bool ShowPreviewFeatures
+        {
+            get
+            {
+                var previewFeatures = ConfigurationManager.AppSettings["PreviewFeatures"];
+                return (previewFeatures != null &&
+                        previewFeatures.Equals("ON", StringComparison.InvariantCultureIgnoreCase));
+            }
+        }
+    }
+}
